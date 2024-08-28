f4a336bbd900e58d97a23137931854eb7fe01988
KarmePz <baezjulicarme16@gmail.com>
2024-08-28 11:20:43 -0300

    Se realizaron cambios en signIn Form

--- Spa Sentirse Bien Web/JS/loginRegistro.js
+++ Spa Sentirse Bien Web/JS/loginRegistro.js
@@ -11,11 +11,18 @@ sectionRegistro.style.display = 'none';
 //EVENTOS DE CLICK
 botonIngreso.addEventListener('click', function(){
     sectionIngreso.style.display = 'block';
+    botonIngreso.style.display ='none';
+    botonRegistro.style.display = 'block';
     sectionRegistro.style.display = 'none';
+    volverBoton.style.display = 'block';
 })
 botonRegistro.addEventListener('click', function(){
     sectionIngreso.style.display = 'none';
+    botonRegistro.style.display = 'none';
+    botonIngreso.style.display ='block';
     sectionRegistro.style.display = 'block';
+    volverBoton.style.display = 'block';
+
 })
 volverBoton.addEventListener('click', function(){
     window.location.href= "../main.html"
