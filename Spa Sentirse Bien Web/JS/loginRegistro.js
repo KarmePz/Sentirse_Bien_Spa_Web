13d33334da8a205f7e841e152322e7a5b76b720e
KarmePz <baezjulicarme16@gmail.com>
2024-09-13 22:16:06 -0300

    se añadio logica de consumo de api

--- Spa Sentirse Bien Web/JS/loginRegistro.js
+++ Spa Sentirse Bien Web/JS/loginRegistro.js
@@ -34,3 +34,59 @@ function activarRegistrarse(){
     sectionRegistro.style.display = 'block';
     volverBoton.style.display = 'block';
 }
+
+
+
+
+
+//Se evita el envio predeterminado de envio:
+document.querySelector('.form_container_ingreso').addEventListener('submit',async function(event){
+    event.preventDefault();
+
+    
+var nombreEmail = document.querySelector('#nombreEmail').value;
+var contrasenia = document.querySelector('#contrasena').value;
+
+//Datos del formulario
+const datosUsuario = {
+    username: nombreEmail,
+    password: contrasenia
+};
+
+//Petición de ApI
+
+    try{
+        //Petición POST
+        const response = await fetch('https://localhost:7034/api/Account/login', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(datosUsuario)
+        });
+
+        //Procesamiento de la respuesta API
+        const resultado = await response.json();
+        if (response.status === 405) {
+            // Manejar error 401: No autorizado
+            console.error("Error 405");
+            alert("Error:Por favor, intenta de nuevo.");
+         }
+        else if(response.ok){
+            //Si hay login, se redirige y se muestra un mensaje por consola
+            console.log("Login Exitoso: ", resultado);
+            //Redirigir
+            window.location.href= "https://www.google.com";
+            //TODO
+        }else{
+            console.error("Error en el login", resultado.message);
+            alert("Error en el login: " + resultado.message);
+        }
+
+    }catch(error){
+        console.error("Hubo un problema al realizar la petición: ", error);
+        alert("Error en la conexión con la API");
+    }
+
+});
